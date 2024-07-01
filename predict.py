86d2ff0731eaafd03304aeca430643c60b644e49
Benjamin Cretois <benjamin.cretois@nina.no>
2024-07-01 10:45:28 +0200

    [ADD] create a json file with all the model detection

--- predict.py
+++ predict.py
@@ -3,6 +3,7 @@ import cv2
 import torch
 import numpy as np
 import yaml
+import json
 from yaml import FullLoader
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
@@ -35,48 +36,86 @@ def predict(image, predictor, tile_size, overlap):
         scores = instances.scores.numpy()
         for box, score in zip(boxes, scores):
             if score >= predictor.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST:
-                results.append((box, x, y))
+                results.append((box, score, x, y))
     return results
 
 
 def visualize_preds(image, results, output_path):
     # Visualize predictions on the original image
-    for box, x_offset, y_offset in results:
+    for box, score, x_offset, y_offset in results:
         x1, y1, x2, y2 = box
         x1 += x_offset
         y1 += y_offset
         x2 += x_offset
         y2 += y_offset
         cv2.rectangle(image, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
+        cv2.putText(
+            image,
+            f"{score:.2f}",
+            (int(x1), int(y1) - 10),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.5,
+            (0, 255, 0),
+            2,
+        )
     # Save the output image
     cv2.imwrite(output_path, image)
     print(f"Output saved to {output_path}")
 
 
+def save_predictions_to_json(results, output_json_path):
+    detections = []
+    for box, score, x_offset, y_offset in results:
+        x1, y1, x2, y2 = box
+        x1 += x_offset
+        y1 += y_offset
+        x2 += x_offset
+        y2 += y_offset
+        detections.append(
+            {
+                "bbox": [float(x1), float(y1), float(x2), float(y2)],
+                "score": float(score),
+            }
+        )
+    with open(output_json_path, "w") as f:
+        json.dump(detections, f, indent=4)
+    print(f"Predictions saved to {output_json_path}")
+    print(f"{len(detections)} detections have been made")
+
+
 def main(image_path, cfg):
 
-    config_file = cfg["CONFIG_FILE"]
-    model_weights = cfg["MODEL_WEIGHTS"]
-    output_path = cfg["OUTPUT_FOLDER"]
-    tile_size = cfg["TILE_SIZE"]
-    overlap = cfg["OVERLAP"]
-    output_path = os.path.join(output_path, os.path.basename(image_path))
+    # Folder management
+    os.makedirs(os.path.join(cfg["OUTPUT_FOLDER"], "image"), exist_ok=True)
+    os.makedirs(os.path.join(cfg["OUTPUT_FOLDER"], "json"), exist_ok=True)
+    output_image_path = os.path.join(
+        cfg["OUTPUT_FOLDER"],
+        "image",
+        f"{os.path.splitext(os.path.basename(image_path))[0]}_pred.png",
+    )
+    output_json_path = os.path.join(
+        cfg["OUTPUT_FOLDER"],
+        "json",
+        f"{os.path.splitext(os.path.basename(image_path))[0]}_pred.json",
+    )
 
     image = cv2.imread(image_path)
 
     # Load the predictor
-    predictor = load_predictor(config_file, model_weights)
+    predictor = load_predictor(cfg["CONFIG_FILE"], cfg["MODEL_WEIGHTS"])
 
     # Perform prediction and visualize the results
-    results = predict(image, predictor, tile_size, overlap)
-    visualize_preds(image, results, output_path)
+    results = predict(image, predictor, cfg["TILE_SIZE"], cfg["OVERLAP"])
+
+    if cfg["PLOT_PREDICTION"]:
+        visualize_preds(image, results, output_image_path)
+
+    save_predictions_to_json(results, output_json_path)
 
 
 if __name__ == "__main__":
-
     with open("./config.yaml") as f:
         cfg = yaml.load(f, Loader=FullLoader)
-    os.makedirs(cfg["OUTPUT_FOLDER"], exist_ok=True)
 
     image_path = sys.argv[1]
 
