f5a5abeb7a48485be4f402574374f3f5efc16155
Taheera Ahmed <taheera.ahmed@nina.no>
2024-10-02 12:53:35 +0200

    [ADD] create test dataset and file for evaluating model

--- src/dataset_utils/__init__.py
+++ src/dataset_utils/__init__.py
@@ -1,3 +1,3 @@
 from .augmentations import build_augmentation
-from .dataset_split import split_dataset
+from .dataset_split import split_dataset, create_test_dataset
 from .dataset_utils import get_reindeer_dicts
