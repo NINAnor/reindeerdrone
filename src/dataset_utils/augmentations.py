e2c77f11ba622fee08fb58ddc3102abad70c5dfc
Taheera Ahmed <taheera.ahmed@nina.no>
2024-10-04 13:33:39 +0200

    [FIX] Augmentations and fixed path

--- src/dataset_utils/augmentations.py
+++ src/dataset_utils/augmentations.py
@@ -3,13 +3,12 @@ import detectron2.data.transforms as T
 def build_augmentation(cfg, is_train=True):
     if is_train:
         augmentation = [
-            T.Resize((800, 800)),
-            T.RandomFlip(prob=0.5, horizontal=True, vertical=False),
-            T.RandomRotation(angle=[-10, 10], expand=False),
-            T.RandomCrop("relative", (0.8, 0.8))
+            T.ResizeShortestEdge(short_edge_length=(640, 672, 704, 736, 768, 800), max_size=1333, sample_style='choice'),
+            T.RandomFlip(),
+            T.RandomLighting(scale=0.9),
         ]
     else:
-        augmentation = [
-            T.Resize((800, 800))
+        augmentation = [ 
+            T.ResizeShortestEdge(short_edge_length=(800, 800), max_size=1333, sample_style='choice')
         ]
     return augmentation
